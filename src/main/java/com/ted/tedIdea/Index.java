b88bb463e29ac322b5677c292e13f078023f05ed
kush <legendkushan@gmail.com>
2018-03-20 15:45:25 -0400

    server up and running

---
+++ src/main/java/com/ted/tedIdea/Index.java
@@ -0,0 +1,16 @@
+package com.ted.tedIdea;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class Index {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(Index.class, args);
+		
+
+	}
+
+}
